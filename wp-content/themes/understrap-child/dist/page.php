32c7c3d07a0fd667299ae50da6473a69eb0f6697
jonwhite100 <jon@bpm-web-design.co.uk>
2019-09-18 21:09:06 +0100

    Sassified most of the about us section plus also amended some template files / index.php. Blog pages now use BS4 card component markup. WIP

--- wp-content/themes/understrap-child/dist/page.php
+++ wp-content/themes/understrap-child/dist/page.php
@@ -35,25 +35,26 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<div id="content" tabindex="-1">
 
 		<div class="row">
-
 			<!-- Do the left sidebar check -->
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
 
 			<main class="site-main" id="main">
+				<div class="container-fluid">
+					<div class="row">
+						<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+							<?php get_template_part( 'loop-templates/content', 'page' ); ?>
 
-					<?php get_template_part( 'loop-templates/content', 'page' ); ?>
+							<?php
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+							?>
 
-					<?php
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-					?>
-
-				<?php endwhile; // end of the loop. ?>
-
+						<?php endwhile; // end of the loop. ?>
+					</div>
+				</div>
 			</main><!-- #main -->
 
 			<!-- Do the right sidebar check -->
