ae0c77ca9314a4c1c43aba4217ee3ea921609612
jon white <jon.white@cwideonline.com>
2019-09-19 14:29:56 +0100

    further work on blog post markup -> BS4. Still WIP

--- wp-content/themes/understrap-child/loop-templates/content.php
+++ wp-content/themes/understrap-child/loop-templates/content.php
@@ -8,49 +8,57 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<div class="col-sm-6 col-md-4 col-lg-3">
+
+<!--
+
+<div class="card" style="width: 18rem;">
+
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+-->
+<div class="col-sm-6 col-md-4 mb-5">
 	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-		<div class="card">
-			<header class="card-header entry-header">
-				<?php
-				the_title(
-					sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-					'</a></h2>'
-				);
-				?>
+		<div class="card card-blog-post shadow">
+            <div class="card-img-top">
+                <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+            </div>
 
-				<?php if ( 'post' == get_post_type() ) : ?>
+            <div class="card-body entry-content p-4">
+                <?php
+                the_title(
+                    sprintf( '<h3 class="card-title entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+                    '</a></h3>'
+                );
+                ?>
 
-					<div class="entry-meta">
-						<?php understrap_posted_on(); ?>
-					</div><!-- .entry-meta -->
+                <?php if ( 'post' == get_post_type() ) : ?>
 
-				<?php endif; ?>
+                    <p class="card-subtitle mb-4 entry-meta">
+                        <?php understrap_posted_on(); ?>
+                    </p><!-- .entry-meta -->
 
-			</header><!-- .entry-header -->
+                <?php endif; ?>
 
-			<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+                <?php the_excerpt(); ?>
 
-			<div class="entry-content">
+                <?php
+                wp_link_pages(
+                    array(
+                        'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+                        'after'  => '</div>',
+                    )
+                );
+                ?>
+            </div>
 
-				<?php the_excerpt(); ?>
-
-				<?php
-				wp_link_pages(
-					array(
-						'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-						'after'  => '</div>',
-					)
-				);
-				?>
-
-			</div><!-- .entry-content -->
-
-			<footer class="entry-footer">
-
-				<?php understrap_entry_footer(); ?>
-
-			</footer><!-- .entry-footer -->
+            <div class="card-footer entry-footer">
+                <?php understrap_entry_footer(); ?>
+            </div>
 		</div>
 	</article><!-- #post-## -->
 </div>
