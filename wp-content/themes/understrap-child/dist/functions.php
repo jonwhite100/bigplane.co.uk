c36295413185390f1b3ab1bcb69cac3ac9fb4625
jonwhite100 <jon@bpm-web-design.co.uk>
2019-09-26 21:54:22 +0100

    missed out the dist

--- wp-content/themes/understrap-child/dist/functions.php
+++ wp-content/themes/understrap-child/dist/functions.php
@@ -63,3 +63,26 @@ function be_dps_current_class( $classes, $post ) {
 	return $classes;
 }
 add_filter( 'display_posts_shortcode_post_class', 'be_dps_current_class', 10, 2 );
+
+
+/**
+* Overriding the function in understrap/inc/setup.php
+*/
+add_filter( 'wp_trim_excerpt', 'understrap_all_excerpts_get_more_link' );
+
+if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
+	/**
+	 * Adds a custom read more link to all excerpts, manually or automatically generated
+	 *
+	 * @param string $post_excerpt Posts's excerpt.
+	 *
+	 * @return string
+	 */
+	function understrap_all_excerpts_get_more_link( $post_excerpt ) {
+		if ( ! is_admin() ) {
+			$post_excerpt = $post_excerpt . ' [...]<p><a class="btn circle border btn-sm" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More',
+			'understrap' ) . '</a></p>';
+		}
+		return $post_excerpt;
+	}
+}
--- wp-content/themes/understrap-child/dist/loop-templates/content.php
+++ wp-content/themes/understrap-child/dist/loop-templates/content.php
@@ -8,49 +8,45 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 ?>
-<div class="col-sm-6 col-md-4 col-lg-3">
+
+<div class="col-sm-6 col-md-4 mb-5">
 	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-		<div class="card">
-			<header class="card-header entry-header">
-				<?php
-				the_title(
-					sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-					'</a></h2>'
-				);
-				?>
+		<div class="card card-blog-post shadow">
+            <div class="card-img-top">
+                <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+            </div>
 
-				<?php if ( 'post' == get_post_type() ) : ?>
+            <div class="card-body entry-content p-4">
+                <?php
+                the_title(
+                    sprintf( '<h3 class="card-title entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+                    '</a></h3>'
+                );
+                ?>
 
-					<div class="entry-meta">
-						<?php understrap_posted_on(); ?>
-					</div><!-- .entry-meta -->
+                <?php if ( 'post' == get_post_type() ) : ?>
 
-				<?php endif; ?>
+                    <p class="card-subtitle mb-4 entry-meta">
+                        <?php understrap_posted_on(); ?>
+                    </p><!-- .entry-meta -->
 
-			</header><!-- .entry-header -->
+                <?php endif; ?>
 
-			<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+                <?php the_excerpt(); ?>
 
-			<div class="entry-content">
+                <?php
+                wp_link_pages(
+                    array(
+                        'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+                        'after'  => '</div>',
+                    )
+                );
+                ?>
+            </div>
 
-				<?php the_excerpt(); ?>
-
-				<?php
-				wp_link_pages(
-					array(
-						'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-						'after'  => '</div>',
-					)
-				);
-				?>
-
-			</div><!-- .entry-content -->
-
-			<footer class="entry-footer">
-
-				<?php understrap_entry_footer(); ?>
-
-			</footer><!-- .entry-footer -->
+            <div class="card-footer entry-footer">
+                <?php understrap_entry_footer(); ?>
+            </div>
 		</div>
 	</article><!-- #post-## -->
 </div>
