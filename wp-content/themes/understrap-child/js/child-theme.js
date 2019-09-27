e54d21f2ef076199438e1e5f47ee1b23681342fb
jon white <jon.white@cwideonline.com>
2019-09-27 14:30:17 +0100

    pulsing button (WIP) removed double quotes from scss files, removed the smoothscroll from custooll from custom js file

--- wp-content/themes/understrap-child/js/child-theme.js
+++ wp-content/themes/understrap-child/js/child-theme.js
@@ -3999,19 +3999,20 @@ jQuery(document).ready(function($) {
 	});
 
     // script to scroll to each section by id
-    $(document).on('click', 'a[href*="#"]:not([href="#"])', function(e) {
-        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-            var hashStr = this.hash.slice(1);
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + hashStr +']');
-            if (target.length) {
-                $('html, body').animate({ scrollTop: target.offset().top }, 1000);
-                window.location.hash = hashStr;
-                return false;
-                e.preventDefault();
-            }
-        }
-    });
+    
+    // $(document).on('click', 'a[href*="#"]:not([href="#"])', function(e) {
+    //     if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+    //         var hashStr = this.hash.slice(1);
+    //         var target = $(this.hash);
+    //         target = target.length ? target : $('[name=' + hashStr +']');
+    //         if (target.length) {
+    //             $('html, body').animate({ scrollTop: target.offset().top }, 1000);
+    //             window.location.hash = hashStr;
+    //             return false;
+    //             e.preventDefault();
+    //         }
+    //     }
+    // });
 
     // script for multi slides per BS carousel
     // $('#carouselExample').on('slide.bs.carousel', function (e) {
