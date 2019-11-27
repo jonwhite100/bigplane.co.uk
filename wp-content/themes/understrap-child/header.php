f5232c09d030c8d65b3696f87be192ecc6a8471e
jonwhite100 <jon@bpm-web-design.co.uk>
2019-11-27 20:40:28 +0000

    changed favicon paths for live. changed the smooth scroll script

--- wp-content/themes/understrap-child/header.php
+++ wp-content/themes/understrap-child/header.php
@@ -21,11 +21,17 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
     <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
-	<!-- favicon -->
+	<!-- favicon local
 	<link rel="apple-touch-icon" sizes="180x180" href="/bigplanemedia.com/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/bigplanemedia.com/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/bigplanemedia.com/favicon-16x16.png">
 	<link rel="manifest" href="/bigplanemedia.com/site.webmanifest">
+	-->
+	<!-- favicon live -->
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
 </head>
 
 <body <?php body_class(); ?>>
