ce05e64097d4862f3d59c9ab2f0ffb5a7869755a
jonwhite100 <jon@bpm-web-design.co.uk>
2020-02-05 18:01:30 +0000

    funuction to remove dashicons added

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -21,8 +21,8 @@ function theme_enqueue_styles() {
 	$the_theme = wp_get_theme();
     wp_enqueue_style( 'child-understrap-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) );
     wp_enqueue_script( 'jquery');
-	wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
-	wp_enqueue_script( 'popper-scripts', get_stylesheet_directory_uri() . '/js/popper.min.js', array(), false);
+	// wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
+	// wp_enqueue_script( 'popper-scripts', get_stylesheet_directory_uri() . '/js/popper.min.js', array(), false);
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
@@ -81,7 +81,7 @@ if ( ! function_exists( 'understrap_all_excerpts_get_more_link' ) ) {
 	 */
 	function understrap_all_excerpts_get_more_link( $post_excerpt ) {
 		if ( ! is_admin() ) {
-			$post_excerpt = $post_excerpt . ' [...]<p><a class="btn circle border btn-sm" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More',
+			$post_excerpt = $post_excerpt . ' <p><a class="btn circle border btn-sm" href="' . esc_url( get_permalink( get_the_ID() ) ) . '">' . __( 'Read More',
 			'understrap' ) . '</a></p>';
 		}
 		return $post_excerpt;
